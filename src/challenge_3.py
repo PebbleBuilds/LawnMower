1b8c35b7630c5ba7ab8d7a965ca0ecfdf63f496b
Brian Cheong <cheongb002@gmail.com>
2023-03-15 11:00:09 -0400

    untested, setting up waypoint follower

--- src/challenge_3.py
+++ src/challenge_3.py
@@ -20,13 +20,67 @@ WAYPOINTS: PoseArray = None
 WAYPOINT_IDX: int = 0
 WAYPOINTS_RECEIVED: bool = False
 
-DRONE_POSE: PoseStamped = None
+DRONE_POSE: PoseStamped = PoseStamped()
+DRONE_POSE.pose.position.x = 0
+DRONE_POSE.pose.position.y = 0
+DRONE_POSE.pose.position.z = 0
+# set orientation to 0
+DRONE_POSE.pose.orientation.w = 1
+DRONE_POSE.pose.orientation.x = 0
+DRONE_POSE.pose.orientation.y = 0
+DRONE_POSE.pose.orientation.z = 0
 
 # bounding volume radius
 RADIUS = 0.5  # meters
 
 # how long to hold position after reaching a waypoint
 HOLD_TIME = 2  # seconds
+
+DEBUG: bool = True
+
+
+class WaypointFollower:
+    def __init__(self, radius: float = 0.5, hold_time: int = 2) -> None:
+        self.waypoint_idx: int = 0
+        self.radius = radius
+        self.hold_time = hold_time
+        self.hold_timer = 0
+        self.last_time = rospy.get_time()
+
+    def update_setpoint(self, drone_pose: PoseStamped):
+        global SETPOINT
+        if not WAYPOINTS_RECEIVED:
+            print("Waiting for waypoints")
+            return None
+
+        # check distance to current waypoint
+        dist = np.linalg.norm(
+            np.array(
+                [
+                    drone_pose.pose.position.x,
+                    drone_pose.pose.position.y,
+                    drone_pose.pose.position.z,
+                ]
+            )
+            - WAYPOINTS[self.waypoint_idx]
+        )
+        if DEBUG:
+            print(f"Distance to waypoint: {dist}")
+        if dist < self.radius:
+            # account for hold time
+            if self.hold_timer < self.hold_time:
+                self.hold_timer += rospy.get_time() - self.last_time
+            else:
+                self.hold_timer = 0
+                self.waypoint_idx += 1
+                self.waypoint_idx %= len(WAYPOINTS)
+                print("Reached waypoint. Moving to next waypoint.")
+
+        # set setpoint to current waypoint
+        SETPOINT.position.x = WAYPOINTS[WAYPOINT_IDX, 0]
+        SETPOINT.position.y = WAYPOINTS[WAYPOINT_IDX, 1]
+        SETPOINT.position.z = WAYPOINTS[WAYPOINT_IDX, 2]
+
 
 # Callback handlers
 def handle_launch():
@@ -44,33 +98,9 @@ def handle_launch():
 
 
 def handle_test():
-    global DRONE_POSE, STATE, SETPOINT, WAYPOINTS_RECEIVED, WAYPOINTS, WAYPOINT_IDX
+    global STATE
     STATE = "Test"
     print("Test Requested.")
-    if not WAYPOINTS_RECEIVED:
-        print("No waypoints received. Holding position.")
-        return
-    # check distance to current waypoint
-    dist = np.linalg.norm(
-        np.array(
-            [
-                DRONE_POSE.pose.position.x,
-                DRONE_POSE.pose.position.y,
-                DRONE_POSE.pose.position.z,
-            ]
-        )
-        - WAYPOINTS[WAYPOINT_IDX]
-    )
-    # TODO check if drone has been close to waypoint for a while
-    if dist < RADIUS:
-        # if close enough, move to next waypoint
-        WAYPOINT_IDX += 1
-        WAYPOINT_IDX %= len(WAYPOINTS)  # wrap around
-        print("Reached waypoint. Moving to next waypoint.")
-    # set setpoint to current waypoint
-    SETPOINT.position.x = WAYPOINTS[WAYPOINT_IDX, 0]
-    SETPOINT.position.y = WAYPOINTS[WAYPOINT_IDX, 1]
-    SETPOINT.position.z = WAYPOINTS[WAYPOINT_IDX, 2]
 
 
 def handle_land():
@@ -151,20 +181,13 @@ def comm_node():
     )
     print("Services, subscribers, publishers initialized")
 
-    while not rospy.is_shutdown():
-        if WAYPOINTS_RECEIVED:
-            print("Waypoints:\n", WAYPOINTS)
+    wf = WaypointFollower(RADIUS, HOLD_TIME)
 
-        # Your code goes here
-        if STATE == "Launch":
-            print("Comm node: Launching...")
-        elif STATE == "Test":
-            print("Comm node: Testing...")
-        elif STATE == "Land":
-            print("Comm node: Landing...")
-        elif STATE == "Abort":
-            print("Comm node: Aborting...")
+    while not rospy.is_shutdown():
+        if STATE == "test":
+            wf.update_setpoint(DRONE_POSE)
         # publish setpoint
+        sp_pub.publish(SETPOINT)
         rospy.sleep(0.2)
 
 
