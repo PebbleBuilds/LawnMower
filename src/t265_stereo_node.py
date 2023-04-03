1be491ce884a568b889e10f87f3801686a709fe9
Brian Cheong <cheongb002@gmail.com>
2023-04-03 16:45:29 -0400

    ready to test stereo code

--- src/t265_stereo_node.py
+++ src/t265_stereo_node.py
@@ -4,42 +4,32 @@ from sensor_msgs.msg import Image
 from threading import Lock
 import cv2
 import numpy as np
+from cv_bridge import CvBridge
 
-def camera_matrix(intrinsics):
-    return np.array(
-        [
-            [intrinsics.fx, 0, intrinsics.ppx],
-            [0, intrinsics.fy, intrinsics.ppy],
-            [0, 0, 1],
-        ]
-    )
-
-
-def fisheye_distortion(intrinsics):
-    return np.array(intrinsics.coeffs[:4])
-
-# TODO initialize IMG1 and IMG2 to empty images
-IMG1 = None
-IMG2 = None
-
-# TODO initialize parameters properly
-INTRINSICS1 = None
-INTRINSICS2 = None
-
-# TODO initialize the following params with the streamed camera info, support arbitrary hardware
-K1 = [286.1167907714844, 0.0, 421.62689208984375, 0.0, 286.27880859375, 399.5252990722656, 0.0, 0.0, 1.0]
-
-K2 = [286.1825866699219, 0.0, 425.0535888671875, 0.0, 286.18560791015625, 398.7244873046875, 0.0, 0.0, 1.0]
-D1 = [-0.008292312733829021, 0.0453055202960968, -0.04263167083263397, 0.007736437954008579]
-D2 = [-0.00701235281303525, 0.04197357967495918, -0.03934945911169052, 0.006663409061729908]
-
-# extrinsics
-R = None
-T = None
-R1 = np.eye(3)
-R2 = R 
 
 H, W = 800, 848
+img_size_WH = (W, H)
+# TODO initialize IMG1 and IMG2 to empty images
+IMG1 = np.zeros((H, W, 3), dtype=np.uint8)
+IMG2 = np.zeros((H, W, 3), dtype=np.uint8)
+
+
+# TODO initialize the following params with the streamed camera info, support arbitrary hardware
+# Camera intrinsics
+K1 = np.array([
+    286.1167907714844,  0.0,                421.62689208984375, 
+    0.0,                286.27880859375,    399.5252990722656, 
+    0.0,                0.0,                1.0
+]).reshape(3, 3)
+K2 = np.array([
+    286.1825866699219,  0.0,                425.0535888671875, 
+    0.0,                286.18560791015625, 398.7244873046875, 
+    0.0,                0.0,                1.0
+]).reshape(3, 3)
+# Distortion params
+D1 = np.array([-0.008292312733829021, 0.0453055202960968, -0.04263167083263397, 0.007736437954008579])
+D2 = np.array([-0.00701235281303525, 0.04197357967495918, -0.03934945911169052, 0.006663409061729908])
+
 
 # TODO put params in launch file
 min_disp = 0
@@ -63,55 +53,44 @@ stereo = cv2.StereoBM_create(
     speckleRange=speckle_range,
 )
 
-stereo_fov_rad = np.pi / 2.0  # 90 degrees
-stereo_height_px = 480
-stereo_focal_px = stereo_height_px / (2.0 * np.tan(stereo_fov_rad / 2.0))
-stereo_width_px = stereo_height_px + max_disp
-stereo_size = (stereo_width_px, stereo_height_px)
-stereo_cx = (stereo_height_px - 1) / 2.0 + max_disp
-stereo_cy = (stereo_height_px - 1) / 2.0
+# From camera infos
+R1 = np.eye(3)
+R2 = np.eye(3)
+P1 = np.array([
+    286.1167907714844,  0.0,                421.62689208984375, 0.0,
+    0.0,                286.27880859375,    399.5252990722656,  0.0, 
+    0.0,                0.0,                1.0,                0.0
+]).reshape(3, 4)
+P2 = np.array([
+    286.1825866699219,  0.0,                425.0535888671875,  -18.2928466796875, 
+    0.0,                286.18560791015625, 398.7244873046875,  -0.01925480365753174, 
+    0.0,                0.0,                1.0,                0.0
+]).reshape(3, 4)
 
-# [286.1167907714844, 0.0, 421.62689208984375, 0.0, 0.0, 286.27880859375, 399.5252990722656, 0.0, 0.0, 0.0, 1.0, 0.0]
-P1 = np.array(
-    [
-        [stereo_focal_px, 0, stereo_cx, 0],
-        [0, stereo_focal_px, stereo_cy, 0],
-        [0, 0, 1, 0],
-    ]
-)
-
-# [286.1825866699219, 0.0, 425.0535888671875, -18.2928466796875, 0.0, 286.18560791015625, 398.7244873046875, -0.01925480365753174, 0.0, 0.0, 1.0, 0.0]
-P2 = P1.copy()
-P2[0, 3] = T[0] * stereo_focal_px
-
-Q = np.array(
-    [
-        [1, 0, 0, -(stereo_cx - max_disp)],
-        [0, 1, 0, -stereo_cy],
-        [0, 0, 0, stereo_focal_px],
-        [0, 0, 1 / T[0], 0],
-    ]   
-)
+T = np.zeros(3)
+T[0] = -P2[0, 3] / P2[0, 0] # 64 mm baseline
 
 m1type = cv2.CV_32FC1
 map1x, map1y = cv2.fisheye.initUndistortRectifyMap(
-    K1, D1, R1, P1, stereo_size, m1type
+    K1, D1, R1, P1, img_size_WH, m1type
 )
 map2x, map2y = cv2.fisheye.initUndistortRectifyMap(
-    K2, D2, R2, P2, stereo_size, m1type
+    K2, D2, R2, P2, img_size_WH, m1type
 )
-
 
 frame_mutex = Lock()
 
+bridge = CvBridge()
+
+
 def callback_fisheye1(msg):
     global IMG1    
-    IMG1 = msg
+    IMG1 = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
 
 def callback_fisheye2(msg):
     global IMG2
-    IMG2 = msg
+    IMG2 = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
 
 def main():
@@ -153,18 +132,13 @@ def main():
         cv2.waitKey(1)
         
         # project disparity to 3D
-        points = cv2.reprojectImageTo3D(disparity, Q)
+        # points = cv2.reprojectImageTo3D(disparity, Q)
         
-        # convert to correct msg type
-        pub_msg = Image()
-        pub_msg.header = header
-        pub_msg.height = points.shape[0]
-        pub_msg.width = points.shape[1]
-        pub_msg.encoding = "32FC3"
-        pub_msg.is_bigendian = False
-        pub_msg.step = points.shape[1] * 3 * 4
-        pub_msg.data = points.tostring()
-        stereo_pub.publish(pub_msg)
+        # # convert to correct msg type
+        # pub_msg = Image()
+        # pub_msg = bridge.cv2_to_imgmsg(points, encoding="passthrough")
+        # pub_msg.header = header
+        # stereo_pub.publish(pub_msg)
 
 
 
