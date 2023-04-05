e7d2f4b8281e0cd11fc494b4a961e9b268ef10ad
drone <cheongb002@gmail.com>
2023-04-05 15:02:13 -0400

    update constant names

--- src/constants.py
+++ src/constants.py
@@ -27,7 +27,7 @@ MAVROS_SETPOINT_TOPIC = "/mavros/setpoint_position/local"
 
 # stereo camera constants
 H, W = 800, 848
-img_size_WH = (W, H)
+IMG_SIZE_WH = (W, H)
 DOWNSCALE = 2
-stereo_size_WH = (W//DOWNSCALE, H//DOWNSCALE)
+STEREO_SIZE_WH = (W//DOWNSCALE, H//DOWNSCALE)
 BASELINE = -18.2928466796875/286.1825866699219 # 64 mm baseline
