cffb9d2237a9c4707e6820b4e1128702a231a858
drone <cheongb002@gmail.com>
2023-04-06 12:24:07 -0400

    update undistort to handle resizing

--- src/undistort_fisheye_node.py
+++ src/undistort_fisheye_node.py
@@ -44,6 +44,10 @@ def img_cb(msg, undistort_pub, mapx, mapy):
         interpolation=cv2.INTER_LINEAR,
         borderMode=cv2.BORDER_CONSTANT,
     )
+    # downscale
+    img_undistorted = cv2.resize(
+        img_undistorted, STEREO_SIZE_WH, interpolation=cv2.INTER_AREA
+    )
     # convert from mono8 to bgr8
     img_undistorted = cv2.cvtColor(img_undistorted, cv2.COLOR_GRAY2BGR)
     output_msg = BRIDGE.cv2_to_imgmsg(img_undistorted, encoding="bgr8")
@@ -53,18 +57,19 @@ def img_cb(msg, undistort_pub, mapx, mapy):
 def camera_info_cb1(msg):
     global CAM_INFO1
     CAM_INFO1 = copy.deepcopy(msg)
-    msg.distortion_model = "plumb_bob"
-    msg.D = [0, 0, 0, 0, 0]
     camera_info_cb(msg, CAMERA_INFO_PUB1)
 
 def camera_info_cb2(msg):
     global CAM_INFO2
     CAM_INFO2 = copy.deepcopy(msg)
-    msg.distortion_model = "plumb_bob"
-    msg.D = [0, 0, 0, 0, 0]
     camera_info_cb(msg, CAMERA_INFO_PUB2)
 
 def camera_info_cb(msg, camera_info_pub):
+    msg.distortion_model = "plumb_bob"
+    msg.D = [0, 0, 0, 0, 0]
+    msg.K = [msg.K[0]/DOWNSCALE, 0, msg.K[2]/DOWNSCALE, 
+             0, msg.K[4]/DOWNSCALE, msg.K[5]/DOWNSCALE,
+               0, 0, 1]
     camera_info_pub.publish(msg)
 
 def init_maps():
@@ -74,16 +79,16 @@ def init_maps():
     K2 = np.array(CAM_INFO2.K).reshape(3,3)
     D2 = np.array(CAM_INFO2.D)
     T = np.array([BASELINE, 0, 0]) # 64 mm baseline
+
     R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(
-        K1, D1, K2, D2, STEREO_SIZE_WH, R=np.eye(3), T=T
+        K1, D1, K2, D2, IMG_SIZE_WH, R=np.eye(3), T=T
     )
     MAPX1, MAPY1 = cv2.fisheye.initUndistortRectifyMap(
-        K1, D1, R1, P1, size=STEREO_SIZE_WH, m1type=cv2.CV_32FC1
+        K1, D1, R1, P1, size=IMG_SIZE_WH, m1type=cv2.CV_32FC1
     )
     MAPX2, MAPY2 = cv2.fisheye.initUndistortRectifyMap(
-        K2, D2, R2, P2, size=STEREO_SIZE_WH, m1type=cv2.CV_32FC1
+        K2, D2, R2, P2, size=IMG_SIZE_WH, m1type=cv2.CV_32FC1
     )
-    rospy.loginfo("Initialized stereo rectification maps")
 
 def main():
     global UNDISTORT_PUB1, CAMERA_INFO_PUB1, UNDISTORT_PUB2, CAMERA_INFO_PUB2
@@ -104,7 +109,7 @@ def main():
     )
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        if CAM_INFO1 is not None and CAM_INFO2 is not None:
+        if CAM_INFO1 is not None and CAM_INFO2 is not None and MAPX1 is None:
             init_maps()
         rate.sleep()
 
