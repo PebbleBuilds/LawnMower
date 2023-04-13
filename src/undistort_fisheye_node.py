e5100faf34f8e903deb394ac111c0f8d9abb1d2f
gerrychen117 <48033988+GerryChen117@users.noreply.github.com>
2023-04-13 17:36:05 -0400

    Synchronization fixes

--- src/undistort_fisheye_node.py
+++ src/undistort_fisheye_node.py
@@ -7,6 +7,7 @@ from threading import Lock
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
+from message_filters import TimeSynchronizer, Subscriber
 
 from constants import *
 import copy
@@ -37,37 +38,70 @@ def img_cb1(msg):
 def img_cb2(msg):
     img_cb(msg, UNDISTORT_PUB2, MAPX2, MAPY2)
 
-def img_cb(msg, undistort_pub, mapx, mapy):
+def img_cb(img_msg1, img_msg2, camera_info_msg1, camera_info_msg2):
     global DROP_IND
-    if any([x is None for x in (mapx, mapy, undistort_pub, CAMERA_INFO_PUB1, CAMERA_INFO_PUB2)]):
+    check_none = (
+        MAPX1, 
+        MAPX2,
+        MAPY1,
+        MAPY2,
+        UNDISTORT_PUB1,
+        UNDISTORT_PUB2,
+        CAMERA_INFO_PUB1,
+        CAMERA_INFO_PUB2,
+    )
+    if any([x is None for x in check_none]):
+        rospy.logwarn("Waiting for camera info")
         return
+    # drop frames to reduce load
     DROP_IND += 1
     if DROP_IND < DROP_FRAMES:
         return
     DROP_IND = 0
-    img_distorted = BRIDGE.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-    img_undistorted = cv2.remap(
-        img_distorted,
-        mapx,
-        mapy,
+
+    img_distorted1 = BRIDGE.imgmsg_to_cv2(img_msg1, desired_encoding="passthrough")
+    img_undistorted1 = cv2.remap(
+        img_distorted1,
+        MAPX1,
+        MAPY1,
+        interpolation=cv2.INTER_LINEAR,
+        borderMode=cv2.BORDER_CONSTANT,
+    )
+    img_distorted2 = BRIDGE.imgmsg_to_cv2(img_msg2, desired_encoding="passthrough")
+    img_undistorted2 = cv2.remap(
+        img_distorted2,
+        MAPX2,
+        MAPY2,
         interpolation=cv2.INTER_LINEAR,
         borderMode=cv2.BORDER_CONSTANT,
     )
     # crop top and bottom based on DOWNSCALE_H
-    orig_height = img_undistorted.shape[0]
+    orig_height = img_undistorted1.shape[0]
     new_height = orig_height//DOWNSCALE_H
+
     # take center of image of new height
-    img_undistorted = img_undistorted[
+    img_undistorted1 = img_undistorted1[
         (orig_height - new_height)//2 : (orig_height + new_height)//2, :
     ]
+    img_undistorted2 = img_undistorted2[
+        (orig_height - new_height)//2 : (orig_height + new_height)//2, :
+    ]
+    
     # convert from mono8 to bgr8
     img_undistorted = cv2.cvtColor(img_undistorted, cv2.COLOR_GRAY2BGR)
-    output_msg = BRIDGE.cv2_to_imgmsg(img_undistorted, encoding="bgr8")
-    output_msg.header = msg.header
-    undistort_pub.publish(output_msg)
-    CAM_INFO1_MODIFIED.header = msg.header
+    output_msg1 = BRIDGE.cv2_to_imgmsg(img_undistorted1, encoding="bgr8")
+    output_msg1.header = img_msg1.header
+    output_msg2 = BRIDGE.cv2_to_imgmsg(img_undistorted2, encoding="bgr8")
+    output_msg2.header = img_msg2.header
+
+    # update camera info
+    CAM_INFO1_MODIFIED.header = camera_info_msg1.header
+    CAM_INFO2_MODIFIED.header = camera_info_msg2.header
+
+    # publish
+    UNDISTORT_PUB1.publish(output_msg1)
+    UNDISTORT_PUB2.publish(output_msg2)
     CAMERA_INFO_PUB1.publish(CAM_INFO1_MODIFIED)
-    CAM_INFO2_MODIFIED.header = msg.header
     CAMERA_INFO_PUB2.publish(CAM_INFO2_MODIFIED)
 
 def camera_info_cb1(msg):
@@ -120,10 +154,22 @@ def main():
     global UNDISTORT_PUB1, CAMERA_INFO_PUB1, UNDISTORT_PUB2, CAMERA_INFO_PUB2
     rospy.init_node("undistort_img_node")
     # subscribers
-    rospy.Subscriber("/camera/fisheye1/image_raw", Image, img_cb1)
-    rospy.Subscriber("/camera/fisheye1/camera_info", CameraInfo, camera_info_cb1)
-    rospy.Subscriber("/camera/fisheye2/image_raw", Image, img_cb2)
-    rospy.Subscriber("/camera/fisheye2/camera_info", CameraInfo, camera_info_cb2)
+    sync = TimeSynchronizer(
+        [
+            Subscriber("/camera/fisheye1/image_raw", Image),
+            Subscriber("/camera/fisheye2/image_raw", Image),
+            Subscriber("/camera/fisheye1/camera_info", CameraInfo), # unused
+            Subscriber("/camera/fisheye2/camera_info", CameraInfo), # unused
+        ],
+        10,
+        0.05,
+    )
+    sync.registerCallback(img_cb)
+    # rospy.Subscriber("/camera/fisheye1/image_raw", Image, img_cb1)
+    # rospy.Subscriber("/camera/fisheye1/camera_info", CameraInfo, camera_info_cb1)
+    # rospy.Subscriber("/camera/fisheye2/image_raw", Image, img_cb2)
+    # rospy.Subscriber("/camera/fisheye2/camera_info", CameraInfo, camera_info_cb2)
+
     # publishers
     UNDISTORT_PUB1 = rospy.Publisher("undistorted1/image_raw", Image, queue_size=1)
     CAMERA_INFO_PUB1 = rospy.Publisher(
